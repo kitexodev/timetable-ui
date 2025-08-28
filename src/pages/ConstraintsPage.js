b0a6fa4af66919262f1cf4a74dd7cd45e728e661
kitexodev <227348372+kitexodev@users.noreply.github.com>
2025-08-29 02:54:56 +0530

    FIX: Update API URL to include /api/ prefix

--- src/pages/ConstraintsPage.js
+++ src/pages/ConstraintsPage.js
@@ -1,8 +1,7 @@
 // src/pages/ConstraintsPage.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const API_URL = 'http://127.0.0.1:8000/api';
+import API_URL from '../apiConfig';
 
 function ConstraintsPage() {
     const [settings, setSettings] = useState({
